181c685e6ed18ba41fdc9911b2b3654f599d9478
JIRATHXT <99694384+JIRATHXT@users.noreply.github.com>
2024-06-17 16:03:21 +0700

    Update W01_01_Example.java

--- W01_01_Example.java
+++ W01_01_Example.java
@@ -6,14 +6,44 @@ public class W01_01_Example {
         Scanner type = new Scanner(System.in);
         boolean loop = true;
         while (loop) {
+
             System.out.print("[Point: " + String.valueOf(point) + "] Type Some Text: ");
             String output = type.nextLine();
 
+            if (point == 40) {
+                System.out.println("YOUR MAX SCORE!!!!");
+                loop = false;
+                type.close();
+            }
+
             switch (output) {
+                case "week": {
+                    int score = 10;
+                    point = point + score;
+                    System.out.println("this is a week 1");
+                    System.out.println("Your Point +" + String.valueOf(score));
+                    break;
+                }
+                case "day": {
+                    int score = 10;
+                    point = point + score;
+                    System.out.println("this is a day 1");
+                    System.out.println("Your Point +" + String.valueOf(score));
+                    break;
+                }
+
+                case "hello": {
+                    int score = 5;
+                    point = point + score;
+                    System.out.println("Java!!");
+                    System.out.println("Your Point +" + String.valueOf(score));
+                    break;
+                }
+
                 case "studentid": {
                     int score = 10;
                     point = point + score;
-                    System.out.println("Answer: 674230013");
+                    System.out.println("Student IDD: 674230013");
                     System.out.println("Your Point +" + String.valueOf(score));
                     break;
                 }
@@ -21,13 +51,13 @@ public class W01_01_Example {
                 case "name": {
                     int score = 5;
                     point = point + score;
-                    System.out.println("Answer: ji");
+                    System.out.println("Name: ji");
                     System.out.println("Your Point +" + String.valueOf(score));
                     break;
                 }
 
                 case "exit": {
-                    System.out.println("Now is exit");
+                    System.out.println("Now is Back To Basic");
                     loop = false;
                     type.close();
                     break;
@@ -36,6 +66,29 @@ public class W01_01_Example {
                 default: {
                     System.out.println("Nothing Event");
                 }
+            }
+        }
+        System.out.println("Hello Java");
+
+        switch (point) {
+            case 5: {
+                System.out.println("Your Score is: OK");
+            }
+
+            case 10: {
+                System.out.println("Your Score is: Nice");
+            }
+
+            case 20: {
+                System.out.println("Your Score is: Great");
+            }
+
+            case 30: {
+                System.out.println("Your Score is: Pretty Good");
+            }
+
+            case 40: {
+                System.out.println("Your Score is: Good Work");
             }
         }
     }
