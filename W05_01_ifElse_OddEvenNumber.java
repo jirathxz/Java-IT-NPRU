92e83f532cfccf0fe377bc7dc9a1bbd2250ec150
jirathx <99694384+jirathxz@users.noreply.github.com>
2024-07-15 10:55:26 +0700

    Add files via upload

---
+++ W05_01_ifElse_OddEvenNumber.java
@@ -0,0 +1,49 @@
+// From jirathx. na kub but เพิ่มเนื้อหาท้ายคาบ
+
+// ดึง class Scanner มาใช้
+import java.util.Scanner;
+
+public class W05_01_ifElse_OddEvenNumber {
+    public static void main(String[] args) {
+        // ตัวแปร -------------------------------------|
+
+        /* ดึง Scanner มาใช้ */
+        Scanner kb = new Scanner(System.in);
+
+        /* รับค่าจากผู้ใช้ */
+        System.out.print("Enter a number: ");
+        int number = kb.nextInt();
+
+        /* หาเศษ */
+        int result = number % 2;
+
+        // เช็คค่า Even & Odd All Case--------------------------|
+        System.out.println("Case 1 --------------\n\n");
+        if (result == 0) {
+            System.out.println(number + " is Number is Even");
+        } else {
+            System.out.println(number + " is Number is Odd");
+        }
+        System.out.println("\n\nCase 2 --------------\n\n");
+        if (result == 1) {
+            System.out.println(number + " is Number is Odd");
+        } else {
+            System.out.println(number + " is Number is Even");
+        }
+        System.out.println("\n\nCase 3 --------------\n\n");
+        if (result != 0) {
+            System.out.println(number + " is Number is Odd");
+        } else {
+            System.out.println(number + " is Number is Even");
+        }
+        System.out.println("\n\nCase 4 --------------\n\n");
+        if (result != 1) {
+            System.out.println(number + " is Number is Even");
+        } else {
+            System.out.println(number + " is Number is Odd");
+        }
+
+        // ปิดการรับค่าจาก Keyboard --------------------|
+        kb.close();
+    }
+}
---
+++ W05_02_ifElseif_PosNegZero.java
@@ -0,0 +1,25 @@
+// PosNegZero ฉบับปรับปรุง
+
+import java.util.Scanner;
+
+public class W05_02_ifElseif_PosNegZero {
+    public static void main(String[] args) {
+        // รับค่าจากแป้นพิมพ์
+        Scanner kb = new Scanner(System.in);
+        System.out.print("Enter The Output: ");
+        int number = kb.nextInt();
+
+        // หากว่า numberมีค่า - เชิงบวก
+        if (number > 0) {
+            System.out.println("Positive");
+        } else if (number < 0) { // หากว่า numberมีค่า - เชิงลบ
+            System.out.println("Nagative");
+        } else if (number == 0) { // หากว่า numberมีค่า - เท่ากับ 0
+            System.out.println("Zero");
+        } else {
+            System.out.println("Invalid Input");
+        }
+
+        kb.close();
+    }
+}
